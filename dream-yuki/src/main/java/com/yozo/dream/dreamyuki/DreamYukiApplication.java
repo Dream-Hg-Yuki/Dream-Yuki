f13f75c36749744a0a4c2597d6872134de1cff51
Dream-Hg-Yuki <75232863+Dream-Hg-Yuki@users.noreply.github.com>
2020-11-30 14:42:23 +0800

    Add files via upload

---
+++ dream-yuki/src/main/java/com/yozo/dream/dreamyuki/DreamYukiApplication.java
@@ -0,0 +1,13 @@
+package com.yozo.dream.dreamyuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamYukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamYukiApplication.class, args);
+	}
+
+}
---
+++ dream-yuki/src/test/java/com/yozo/dream/dreamyuki/DreamYukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yozo.dream.dreamyuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamYukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
